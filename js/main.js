95485ccc0c484f851067334f9365a3693210845d
Stella Ling <stellalph2@gmail.com>
2022-06-28 22:33:14 +0930

    add code to menu.scss and main.scss

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // Select DOM Items
-const menuBtn = document.squerySelector('.btn-menu');
-const menu = document.squerySelector('.menu');
-const menuNav = document.squerySelector('.menu-nav');
-const menuBranding = document.squerySelector('.menu-branding');
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
 // Set Initial State of Menu
@@ -17,7 +17,18 @@ function toggleMenu() {
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
-    } else {
+
         // Set Menu State
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        // Set Menu State
+        showMenu = false;
+
     }
 }
