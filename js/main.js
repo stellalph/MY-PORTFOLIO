4689c324738187bc97fa5dc940932bd702434ac2
Stella Ling <stellalph2@gmail.com>
2022-06-28 21:28:33 +0930

    add code for main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 // Select DOM Items
-
 const menuBtn = document.squerySelector('.btn-menu');
 const menu = document.squerySelector('.menu');
 const menuNav = document.squerySelector('.menu-nav');
 const menuBranding = document.squerySelector('.menu-branding');
-const navItems = document.querySelectorAll('.nav-items');
+const navItems = document.querySelectorAll('.nav-item');
 
+// Set Initial State of Menu
+let showMenu = false;
 
+menuBtn.addEventListener('click', toggleMenu);
 
+function toggleMenu() {
+    if (!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+    } else {
+        // Set Menu State
+    }
+}
