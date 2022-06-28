eb6bef153a6c7e6dc4b22411bc3ec395c1d8e83e
Stella Ling <stellalph2@gmail.com>
2022-06-28 18:05:27 +0930

    add code main.css, main.js and menu.scss

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Select DOM Items
+
+const menuBtn = document.squerySelector('.btn-menu');
+const menu = document.squerySelector('.menu');
+const menuNav = document.squerySelector('.menu-nav');
+const menuBranding = document.squerySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-items');
+
+
+
