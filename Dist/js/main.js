01c462975adae00efe5efe562c5d5481a6bf7de3
Stella Ling <stellalph2@gmail.com>
2022-07-03 10:30:23 +0930

    add comments to code

--- Dist/js/main.js
+++ Dist/js/main.js
@@ -1,11 +1,11 @@
-// Select DOM Items
+// DOM Items
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
-// Set Initial State of Menu
+// Start Menu
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
@@ -18,7 +18,7 @@ function toggleMenu() {
         menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
 
-        // Set Menu State
+        // Close Menu
         showMenu = true;
     } else {
         menuBtn.classList.remove('close');
@@ -27,7 +27,7 @@ function toggleMenu() {
         menuBranding.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
 
-        // Set Menu State
+        // Menu State
         showMenu = false;
 
     }
